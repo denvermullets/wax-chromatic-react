6ef35b04709e9981330b699f61f5718057e812fa
devmullets <devmullets@gmail.com>
2020-07-05 08:53:25 -0400

    currently working, search for artist and view all main release vinyls

--- src/containers/ArtistProfile.js
+++ src/containers/ArtistProfile.js
@@ -12,6 +12,7 @@ const discogsHeaders = {
   "Content-Type": "application/json",
   "Authorization": `Discogs ${discogsKey} ${discogsSecret}`,
   "User-Agent": "WaxChromatics/v0.1 +https://localhost:3001/login",
+  // "Accept": "application/vnd.discogs.v2.plaintext+json"
 }
 
 const requestOptions = {
@@ -22,9 +23,12 @@ const requestOptions = {
 const ArtistProfile = (props) => {
   const artistId = props.location.state.artist.id // from router push
 
-  const [ releasesLoaded, setReleasesLoaded ] = useState(false)
+  const [ artistInfoLoaded, setArtistInfoLoaded ] = useState(false)
   const [ artistInfo, setArtistInfo ] = useState()
+  const [ releasesLoaded, setReleasesLoaded ] = useState(false)
   const [ releases, setReleases ] = useState()
+  const [ waxReleasesLoaded, setWaxReleasesLoaded ] = useState(false)
+  const [ waxReleases, setWaxReleases ] = useState()
   const [ vinyls, setVinyls ] = useState([])
 
   const checkArtist = () => {
@@ -33,10 +37,11 @@ const ArtistProfile = (props) => {
     fetch(`${waxUrl}/artists/${artistId}`)
       .then(response => response.json())
       .then(artistInfo => setArtistInfo(artistInfo))
+      .then(setArtistInfoLoaded(true))
   }
 
   const getArtistReleases = () => {
-    // get first page of artist releases and then filter out non parent (master) releases
+    // get first page of artist releases and then filter out non parent (master) releases and only albums that are mainly by artist (no collabs cur)
     fetch(`${url}/artists/${artistId}/releases?sort=year&sort_order=desc&page=1&per_page=100`, requestOptions)
       .then(response => response.json())
       .then(artistReleases => {
@@ -44,6 +49,15 @@ const ArtistProfile = (props) => {
         foundReleases = foundReleases.filter(singleRelease => singleRelease.type === 'master' && singleRelease.role === 'Main')
         setReleases(foundReleases)
       })
+      .then(setReleasesLoaded(true))
+      .catch(error => console.log('error', error));
+  }
+
+  const getParentReleases = () => {
+    // get artist releases from WaxDB to compare and see if new albums exist
+    fetch(`${waxUrl}/releases/${artistId}`)
+      .then(response => response.json())
+      .then(artistReleases => setWaxReleases(artistReleases))
       .then(setReleasesLoaded(true))
       .catch(error => console.log('error', error));
   }
@@ -56,23 +70,62 @@ const ArtistProfile = (props) => {
         let findVinyls = releaseInfo.versions
         console.log(`all ${findVinyls[0].title} variants`, findVinyls)
         findVinyls = findVinyls.filter(vinyl => vinyl.major_formats.includes('Vinyl'))
-        findVinyls.map(vinyl => setVinyls(vinyls => [...vinyls, vinyl]))        
+        // map thru array of vinyls from selected release
+        findVinyls.map(vinyl => setVinyls(vinyls => [...vinyls, vinyl]))
+
+           
     })
     .catch(error => console.log('getVinylAlbums oopsie whatsie', error))
   }
 
   const checkWaxRecords = (releaseId) => {
-    fetch(`${waxUrl}/albums/${releaseId}`)
+    fetch(``)
       .then(response => response.json())
       .then(console.log('checked waxChromatics id: ', releaseId))
+      
       // .then(artistInfo => setArtistInfo(artistInfo))
   }
 
+const loadVinyl = () => {
+  console.log('starting que?')
+  // getVinylAlbums(releases[0].id) // start loading 1 album ASAP before 1 second counter starts
+  const throttle = throttledQueue(1, 1500); // at most make 1 request every second.
+  for (let x = 0; x < releases.length; x++) {
+    throttle(function() {
+      // make a network request.
+      getVinylAlbums(releases[x].id)
+    });
+  }
+}
+
+const compareLists = () => {
+  if (releases.length > waxReleases.length) {
+    return true
+  } else {
+    return false
+  }
+}
+
+ //// discogs api limits to 60 calls per second and the api endpoints don't have all the information i'd like
+ //// it ends up taking like 160~ calls to display like 10 albums, it's kinda nutty
+    // flow is:
+      // get artist bio (first checks WaxDB then hits Discogs if no bio exists)
+      // get artist releases (currently just 'masters') from discogs (also contains temp album image url)
+      // get artist releases from WaxDB and then compare to see if new album parent ('master') exists
+        // currently unable to see if there is a new repress w/o digging thru each actual release, could implement user button to check for updates
+        // if new vinyls => get vinyl information and update db
+      // pull up each 'master' release which contains info on all variants and filter out non vinyls
+      // load all vinyls into 'vinyls'
+
   useEffect(() => {
     checkArtist()
-    getArtistReleases()
-  }, [])
-
+    // discogs is so finicky with their api and sometimes i get a 429 response for, what i assume is, too many req coming from
+    // my frontend and backend at the same time. hilarious considering backend only makes 1 req, but w/e
+    if (artistInfoLoaded) {
+      getArtistReleases() // get list of releases from discogs
+      // getParentReleases() // get list of releases from WaxChromatics to compare
+    }
+  }, [artistInfoLoaded])
 
   useEffect(() => {
     if (releasesLoaded){
@@ -80,37 +133,28 @@ const ArtistProfile = (props) => {
     }
   }, [releases])
 
-const loadVinyl = () => {
-  console.log('starting que?')
-  // let's check internal API first to see if info exists
+  // once discogs release list and WaxDb list is compiled, compare to see if we need to cycle thru
+  // useEffect(() => {
+  //   if(releasesLoaded && waxReleasesLoaded) {
+
+  //   }
+  // }, releasesLoaded, waxReleasesLoaded)
 
 
-
-    getVinylAlbums(releases[0].id) // start loading 1 album ASAP before 1 second counter starts
-  const throttle = throttledQueue(1, 1000); // at most make 1 request every second.
-  for (let x = 1; x < releases.length; x++) {
-    throttle(function() {
-        // make a network request.
-        getVinylAlbums(releases[x].id)
-    });
-  }
-}
-
+  let sortedVinyls = vinyls.sort((a,b) => { return a.released < b.released ? -1 : 1 })
   return (
-    
     <>
-      
       <ArtistBio 
         name={artistInfo ? artistInfo.name : null}
         profile={artistInfo ? artistInfo.bio : null}
       />
 
       <AlbumGrid 
-        vinyls={vinyls ? vinyls : null}
+        // vinyls={vinyls ? vinyls : null}
+        vinyls={sortedVinyls ? sortedVinyls : null}
         artistInfo={artistInfo ? artistInfo : null}
       /> 
 
-      {/* {useGetVinyls()} */}
     </>
   );
 }
