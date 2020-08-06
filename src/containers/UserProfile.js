29b12b624678edebfc8a278567df3f9f86a000f8
denvermullets <devmullets@gmail.com>
2020-08-06 13:54:02 -0400

    :sparkles: finish oauth login

--- src/containers/UserProfile.js
+++ src/containers/UserProfile.js
@@ -7,9 +7,10 @@ const UserProfile = () => {
   
   
   useEffect(() => {
+    const waxChromatics = JSON.parse(localStorage.getItem("waxChromatics"))
     let parsed = queryString.parse(window.location.search)
     console.log(parsed)
-    fetch(`${waxUrl}/users/authenticate?oauth_token=${parsed.oauth_token}&oauth_verifier=${parsed.oauth_verifier}`)
+    fetch(`${waxUrl}/users/authenticate?oauth_token=${parsed.oauth_token}&oauth_verifier=${parsed.oauth_verifier}&oauth_token_secret=${waxChromatics.oauth_token_secret}`)
       .then(response => response.json())
       .then(json => console.log(json))
   }, []);
